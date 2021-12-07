7beee22f58c7ddc595a7c3e61cba7bc88ba635b6
Marcin.Habzda <marcin.habzda@miquido.com>
2021-12-07 10:39:53 +0100

    Initialize Android project

---
+++ app/src/main/java/com/mqhabzda/renovate/gradle/issue/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mqhabzda.renovate.gradle.issue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
